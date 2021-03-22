0ae1f9a964d37d2e74639749cf28631d0400b405
方镇东 <fangzhendong@yiwise.com>
2021-03-22 18:55:13 +0800

    fix: 移除dist目录

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const noop: () => void;
-export { isType, isObject, isArray, isFunction, isNumber, isString, isBoolean, isNull, isUndefined, isEmptyObj, } from './type';
-export { cookies } from './cookie';
-export { debounce } from './debounce-throttle';
-export { getPercentNum, getPercentStr, getFileNameFromOssUrl, getFileSuffix, } from './format';
-export { copyText, downloadImage, downloadURL, generateRandom, } from './oper';
